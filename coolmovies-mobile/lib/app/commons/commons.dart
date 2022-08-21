542d55eaad912d6c5c0833f10e4bcfa6ff7918a7
VIctor Teixeira <avictortcosta@gmail.com>
2022-08-21 10:08:06 -0300

    Moved UserRepository to auth module

--- coolmovies-mobile/lib/app/commons/commons.dart
+++ coolmovies-mobile/lib/app/commons/commons.dart
@@ -1,3 +1,2 @@
 export 'package:coolmovies/app/commons/domain/entities/enums.dart';
 export 'package:coolmovies/app/commons/domain/entities/user_entity.dart';
-export 'package:coolmovies/app/commons/external/user_repository.dart';
